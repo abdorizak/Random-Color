1166073facc33fc73f11a58b1e0c9f584ccd15d9
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-10-19 12:07:10 +0300

    Initial Commit

---
+++ Random Colors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Colors
+//
+//  Created by Abdirizak Hassan on 10/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
